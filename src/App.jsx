4389ec48badd89dc6771aea60a312d1e7f2da7c4
Vick Alexa <142795784+Vickabu@users.noreply.github.com>
2025-05-18 21:00:03 +0200

    added venue detail page

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,16 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MainLayout from "./components/MainLayout";
 import HomePage from "./pages/home";
 import Profile from "./pages/profile";
+import VenueDetail from "./components/VenueDetail";
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* MainLayout som wrap rundt hele appen */}
         <Route element={<MainLayout />}>
-          {/* Definer sider som vises inni MainLayout */}
           <Route path="/" element={<HomePage />} />
           <Route path="/profile" element={<Profile />} />
+          <Route path="/venues/:id" element={<VenueDetail />} />
         </Route>
       </Routes>
     </Router>
