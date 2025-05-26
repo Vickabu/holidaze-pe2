232e2a92bedf2d2f31acadb05b01c056ab0ca8e2
Vick Alexa <142795784+Vickabu@users.noreply.github.com>
2025-05-26 14:27:48 +0200

    styled footer

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import { FaTwitter, FaInstagram, FaLinkedin, FaFacebook } from 'react-icons/fa';
 import DarkModeToggle from '../common/DarkModeToggle';
+import { useNavigate } from 'react-router-dom';
 
 export default function Footer() {
   const [isVisible, setIsVisible] = useState(false);
@@ -20,19 +21,22 @@ export default function Footer() {
     return () => window.removeEventListener('scroll', handleScroll);
   }, []);
 
+  const navigate = useNavigate();
   return (
     <footer
       id="footer"
       className={`transition-opacity duration-700 py-8 mt-10 ${isVisible ? 'opacity-100' : 'opacity-0'} 
-        dark:bg-gray-900 text-gray-800 bg-white dark:text-white`}>
-      <div className="max-w-7xl mx-auto px-4">
+        bg-[#F4E9DC] dark:bg-[#1F3B3C] text-[#1F3B3C] dark:text-[#F4E9DC]`}>
+      <div className="max-w-5xl mx-auto px-4">
         <div className="flex flex-col md:flex-row justify-between items-center">
-          {/* Logo - have to find one */}
-          <div className="text-2xl font-bold text-gray-800 dark:text-white mb-4 md:mb-0">
-            Holidaze
-          </div>
+          <img
+            src={"./Holidaze-logo.png"}
+            alt="Holidaze logo"
+            className="w-32 cursor-pointer dark:bg-[#F4E9DC] rounded"
+            onClick={() => navigate("/")}
+          />
 
-          <div className="flex space-x-6 mb-4 md:mb-0">
+          <div className="flex space-x-6 mb-4 md:mb-0 sm:mt-4">
             <a href="#" className="text-gray-800 dark:text-gray-300 hover:text-gray-400 dark:hover:text-gray-500">
               <FaTwitter size={24} />
             </a>
@@ -46,22 +50,10 @@ export default function Footer() {
               <FaFacebook size={24} />
             </a>
           </div>
-
-          <div className="flex space-x-6 mb-4 md:mb-0">
-            <a href="/about" className="text-gray-800 dark:text-gray-300 hover:text-gray-400 dark:hover:text-gray-500">About Us</a>
-            <a href="/privacy" className="text-gray-800 dark:text-gray-300 hover:text-gray-400 dark:hover:text-gray-500">Privacy Policy</a>
-            <a href="/terms" className="text-gray-800 dark:text-gray-300 hover:text-gray-400 dark:hover:text-gray-500">Terms of Service</a>
-          </div>
-
-          {/* Not sure I should keep*/}
-          <div className="mt-4 md:mt-0">
-            <DarkModeToggle />
-          </div>
         </div>
 
-        {/* Copyright he he */}
         <div className="text-center text-sm text-gray-400 mt-6 dark:text-gray-400">
-          &copy; {new Date().getFullYear()} Holidaze. All rights reserved.
+          &copy; {new Date().getFullYear()} Holidaze.
         </div>
       </div>
     </footer>
