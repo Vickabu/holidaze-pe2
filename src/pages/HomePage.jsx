593a73c53497964198af172dd82c7a315df2ff44
Vick Alexa <142795784+Vickabu@users.noreply.github.com>
2025-05-28 22:44:53 +0200

    added h2 to homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,8 +5,11 @@ const HomePage = () => {
   return (
     <div className="max-w-screen-xl mx-auto px-4 mt-10">
       <h1 className="text-4xl font-bold text-center text-gray-900 mb-8">
-        Explore Our Venues
+        Welcome!
       </h1>
+      <h2 className="text-3xl font-bold text-center text-gray-900 mb-8">
+        Explore Our Venues
+      </h2>
 
       <VenueGrid />
     </div>
