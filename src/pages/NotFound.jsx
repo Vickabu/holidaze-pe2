2b2a8fda571558b8371c5d2355d2bb9a0933467e
Vick Alexa <142795784+Vickabu@users.noreply.github.com>
2025-05-28 23:23:02 +0200

    translated not fount to english

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -5,9 +5,9 @@ export default function NotFound() {
   return (
     <div className="text-center mt-20">
       <h1 className="text-5xl font-bold mb-4">404</h1>
-      <p className="mb-6">Oops! Siden du leter etter finnes ikke.</p>
+      <p className="mb-6">Oops! The page you are looking for does not exist.</p>
       <Link to="/" className="text-[#1F3B3C] underline">
-        Gå til forsiden
+        Head back to HomePage
       </Link>
     </div>
   );
