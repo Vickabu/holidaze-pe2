3a84184037c7e3537e6f058baaa29a2a0cd5f4d8
Vick Alexa <142795784+Vickabu@users.noreply.github.com>
2025-05-28 23:27:40 +0200

    Update NotFound.jsx

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -5,7 +5,7 @@ export default function NotFound() {
   return (
     <div className="text-center mt-20">
       <h1 className="text-5xl font-bold mb-4">404</h1>
-      <p className="mb-6">Oops! The page you are looking for does not exist.</p>
+      <p className="mb-6">Oops! The page you are looking for does not exist..</p>
       <Link to="/" className="text-[#1F3B3C] underline">
         Head back to HomePage
       </Link>
